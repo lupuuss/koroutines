bf54384e092d5609481609b43608b88f3623acce
lupuuss <damian.baczynski@gmail.com>
2022-01-16 01:58:08 +0100

    1.2 Dispatchers

--- src/main/kotlin/com/daftmobile/koroutines/1 Dispatchers.kt
+++ src/main/kotlin/com/daftmobile/koroutines/1 Dispatchers.kt
@@ -2,22 +2,22 @@
 @file:OptIn(ExperimentalStdlibApi::class)
 package com.daftmobile.koroutines
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun `1 Dispatchers`() = runBlocking {
     var count = 0
 
-    val job1 = launch {
-        repeat(1_000_000) { count++ }
-    }
+    coroutineScope {
+        launch {
+            repeat(1_000_000) { count++ }
+        }
 
-    val job2 = launch {
-        repeat(1_000_000) { count++ }
+        launch {
+            repeat(1_000_000) { count++ }
+        }
     }
-
-    job1.join()
-    job2.join()
 
     println(count)
 }

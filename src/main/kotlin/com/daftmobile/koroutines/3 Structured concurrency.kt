fe64c2de28a6fc2ad4b131d90cc4e4edbf036de3
lupuuss <damian.baczynski@gmail.com>
2022-01-17 00:41:31 +0100

    3.1 Structured concurrency

--- src/main/kotlin/com/daftmobile/koroutines/3 Structured concurrency.kt
+++ src/main/kotlin/com/daftmobile/koroutines/3 Structured concurrency.kt
@@ -5,13 +5,15 @@ package com.daftmobile.koroutines
 import kotlinx.coroutines.*
 
 fun `3 Structured concurrency`() = runBlocking(Dispatchers.Default) {
-    launch {
-        println(1)
-    }
-    launch {
-        delay(1000)
-        println(2)
+    val job = launch {
+        launch {
+            println(1)
+        }
+        launch {
+            delay(1000)
+            println(2)
+        }
     }
     delay(500)
-    cancel()
+    job.cancel()
 }

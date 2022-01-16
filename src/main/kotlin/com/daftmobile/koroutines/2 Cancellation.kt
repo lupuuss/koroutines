e8a903837c425636d0a9b1c938a6c9e4cfaeb926
lupuuss <damian.baczynski@gmail.com>
2022-01-16 02:23:18 +0100

    2.1 Cancellation

--- src/main/kotlin/com/daftmobile/koroutines/2 Cancellation.kt
+++ src/main/kotlin/com/daftmobile/koroutines/2 Cancellation.kt
@@ -4,11 +4,12 @@ package com.daftmobile.koroutines
 
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 
 fun `2 Cancellation`() = runBlocking {
     val job = launch {
         while (true) { println("XD") }
     }
-
+    yield()
     job.cancel()
 }

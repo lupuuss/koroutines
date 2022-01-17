e7527d988df3872a65100caf1f696f9b39851bdf
lupuuss <damian.baczynski@gmail.com>
2022-01-17 01:53:32 +0100

    4.2 Coroutines API

--- src/main/kotlin/com/daftmobile/koroutines/4 Coroutines API.kt
+++ src/main/kotlin/com/daftmobile/koroutines/4 Coroutines API.kt
@@ -5,7 +5,7 @@ package com.daftmobile.koroutines
 import kotlinx.coroutines.*
 
 fun `4 Coroutines API`() = runBlocking {
-    val job = launch(start = CoroutineStart.ATOMIC) {
+    val job = launch(start = CoroutineStart.UNDISPATCHED) {
         print("Hello ")
         delay(100)
         println(" world!")

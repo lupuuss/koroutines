7a2590f4425a4d089ec205d9ec05b79a3da6bcb4
lupuuss <damian.baczynski@gmail.com>
2022-01-17 01:31:27 +0100

    3.9 Structured concurrency

--- src/main/kotlin/com/daftmobile/koroutines/3 Structured concurrency.kt
+++ src/main/kotlin/com/daftmobile/koroutines/3 Structured concurrency.kt
@@ -6,17 +6,19 @@ import kotlinx.coroutines.*
 
 fun `3 Structured concurrency`() {
     val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
-    scope.launch(SupervisorJob()) {
-        launch {
-            delay(100)
-            throw IllegalStateException("BOOOOOOOOOOM!")
-        }
-        launch {
+    scope.launch {
+        supervisorScope {
+            launch {
+                delay(100)
+                throw IllegalStateException("BOOOOOOOOOOM!")
+            }
+            launch {
+                delay(1000)
+                println("Ready!")
+            }
             delay(1000)
-            println("Ready!")
+            println("Done!")
         }
-        delay(1000)
-        println("Done!")
     }
     scope.launch {
         delay(1000)

d686aa0358fe40c24ed061b8029af7ae8057d3e7
lupuuss <damian.baczynski@gmail.com>
2022-01-17 03:26:37 +0100

    4.12 Coroutines API

--- src/main/kotlin/com/daftmobile/koroutines/4 Coroutines API.kt
+++ src/main/kotlin/com/daftmobile/koroutines/4 Coroutines API.kt
@@ -2,36 +2,40 @@
 @file:OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
 package com.daftmobile.koroutines
 
-import kotlinx.coroutines.*
-import java.lang.IllegalStateException
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeout
 import kotlin.concurrent.thread
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 
 
-fun `4 Coroutines API`() = runBlocking<Unit> {
+fun `4 Coroutines API`() = runBlocking {
     withTimeout(700) {
         println(coRequest(2))
         println(coRequest(1))
     }
 }
 
-suspend fun coRequest(input: Int): Int = suspendCoroutine { continuation ->
-    request(
+suspend fun coRequest(input: Int): Int = suspendCancellableCoroutine { continuation ->
+    val disposable = request(
         input,
         onSuccess = { continuation.resume(it) },
         onError = { continuation.resumeWithException(it) }
     )
+    continuation.invokeOnCancellation { disposable.interrupt() }
 }
 
-fun request(input: Int, onSuccess: (Int) -> Unit, onError: (Exception) -> Unit) {
-    thread {
-        Thread.sleep(1000)
-        if (input == 1)  {
-            onError(IllegalStateException("BOOM!"))
-        } else {
-            onSuccess(input * 37)
+fun request(input: Int, onSuccess: (Int) -> Unit, onError: (Exception) -> Unit): Thread {
+    return thread {
+        runCatching {
+            Thread.sleep(1000)
+            if (input == 1)  {
+                onError(IllegalStateException("BOOM!"))
+            } else {
+                onSuccess(input * 37)
+            }
         }
     }
 }
